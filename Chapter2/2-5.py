d2b267d4c0eb5b96c816ac013c19adf257ae8e59
nao <nao@naonoMacBook-Pro.local>
2021-05-09 23:13:17 +0900

    done: chapter2

---
+++ Chapter2/2-5.py
@@ -0,0 +1,31 @@
+#Creating a Doc
+from spacy.lang.en import English
+
+nlp = English()
+
+# Import the Doc class
+from spacy.tokens import Doc
+
+# Desired text: "spaCy is cool!"
+words = ["spaCy", "is", "cool", "!"]
+spaces = [True, True, False, False]
+
+# Create a Doc from the words and spaces
+doc = Doc(nlp.vocab, words=words, spaces=spaces)
+print(doc.text)
+
+#part2
+from spacy.lang.en import English
+
+nlp = English()
+
+# Import the Doc class
+from spacy.tokens import Doc
+
+# Desired text: "Go, get started!"
+words = ["Go", ",", "get", "started", "!"]
+spaces = [False, True, True, False, False]
+
+# Create a Doc from the words and spaces
+doc = Doc(nlp.vocab, words=words, spaces=spaces)
+print(doc.text)
