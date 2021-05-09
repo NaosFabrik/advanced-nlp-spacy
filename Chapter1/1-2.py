eaac17017fcd75e41a6ca0cae7dbcad2bad04467
nao <nao@naonoMacBook-Pro.local>
2021-05-09 13:42:49 +0900

    initial commit

---
+++ Chapter1/1-2.py
@@ -0,0 +1,24 @@
+#1-2
+# Import the English language class
+from spacy.lang.en import English
+
+# Create the nlp object
+nlp = English()
+
+# Process a text
+doc = nlp("This is a sentence.")
+
+# Print the document text
+print(doc.text)
+
+# Import the German language class
+from spacy.lang.de import German
+​
+# Create the nlp object
+nlp = German()
+​
+# Process a text (this is German for: "Kind regards!")
+doc = nlp("Ich glaube dass heute ist der Europatag!")
+​
+# Print the document text
+print(doc.text)
